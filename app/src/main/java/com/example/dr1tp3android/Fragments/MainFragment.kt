a0ba4f0afd0aee18f5ac939e728611242b35536f
Willian-Notebook <willian.silva@al.infnet.edu.br>
2020-03-20 16:59:23 -0300

    First Commit

---
+++ app/src/main/java/com/example/dr1tp3android/Fragments/MainFragment.kt
@@ -0,0 +1,49 @@
+package com.example.dr1tp3android.Fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+
+import com.example.dr1tp3android.R
+import com.example.dr1tp3android.ViewModel.InvestidorViewModel
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    private lateinit var investidor: InvestidorViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.let {
+            investidor = ViewModelProviders.of(it).get(InvestidorViewModel::class.java)
+        }
+
+        btnIniciar.setOnClickListener{
+            investidor.nome = txtNome.text.toString()
+
+            if(investidor.nome == ""){
+                Toast.makeText(activity, "ERRO: Digite seu nome por favor!",Toast.LENGTH_SHORT).show()
+            }
+            else{
+                findNavController().navigate(R.id.mainFragment)
+            }
+        }
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/dr1tp3android/ViewModel/InvestidorViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.dr1tp3android.ViewModel
+
+import androidx.lifecycle.ViewModel
+
+class InvestidorViewModel: ViewModel() {
+    var nome: String = "null"
+    var acumulador: Int = 0
+}
