7b1b6744d6c19367e19d69fd2141478edfec22b5
Willian-Notebook <willian.silva@al.infnet.edu.br>
2020-03-21 15:45:50 -0300

    Pagina 3 concluída

--- app/src/main/java/com/example/dr1tp3android/Fragments/MainFragment.kt
+++ app/src/main/java/com/example/dr1tp3android/Fragments/MainFragment.kt
@@ -10,7 +10,6 @@ import android.widget.Toast
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.example.dr1tp3android.PerguntasActivity
-
 import com.example.dr1tp3android.R
 import com.example.dr1tp3android.ViewModel.InvestidorViewModel
 import kotlinx.android.synthetic.main.fragment_main.*
@@ -25,7 +24,11 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+
+
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/dr1tp3android/Fragments/Pergunta3Fragment.kt
+++ app/src/main/java/com/example/dr1tp3android/Fragments/Pergunta3Fragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 
 import com.example.dr1tp3android.R
 import com.example.dr1tp3android.ViewModel.InvestidorViewModel
+import kotlinx.android.synthetic.main.fragment_pergunta3.*
 
 /**
  * A simple [Fragment] subclass.
@@ -21,6 +25,33 @@ class Pergunta3Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_pergunta3, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.let {
+            investidor = ViewModelProviders.of(it).get(InvestidorViewModel::class.java)
+        }
+
+        btnProximo3.setOnClickListener{
+            if(respostaA3.isChecked){
+                investidor.acumulador += 0
+                findNavController().navigate(R.id.pergunta2Fragment)
+            }else if(respostaB3.isChecked){
+                investidor.acumulador += 1
+                findNavController().navigate(R.id.pergunta2Fragment)
+            }else if(respostaC3.isChecked){
+                investidor.acumulador += 2
+                findNavController().navigate(R.id.pergunta2Fragment)
+            }else if(respostaD3.isChecked){
+                investidor.acumulador += 4
+                findNavController().navigate(R.id.pergunta2Fragment)
+            }else{
+                Toast.makeText(context,
+                    "Por favor escolha uma alternativa",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
